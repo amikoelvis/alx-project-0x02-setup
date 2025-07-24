5899f3d0c79e85479c348792be18cc1f10911f20
Amiko Elvis <amikoelvis@gmail.com>
2025-07-24 13:05:06 +0300

    Update pages/index.tsx and Header.tsx with content

---
+++ alx-project-0x02/components/layout/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header: React.FC = () => (
+  <header className="bg-blue-500 p-4 text-white">
+    <h1 className="text-2xl">Welcome to My Next.js Project!</h1>
+  </header>
+)
+
+export default Header;
--- alx-project-0x02/pages/index.tsx
+++ alx-project-0x02/pages/index.tsx
@@ -1,6 +1,11 @@
-const Home: React.FC = () => (
-  <div className="p-6">
-      <h1 className="text-xl">Welcome to my Next.js + TypeScript + Tailwind CSS Project!</h1>
+import Header from '../components/layout/Header'
+
+const Home = () => (
+  <div>
+    <Header />
+    <main className="p-6">
+      <h2 className="text-xl">Welcome to my Next.js + TypeScript + Tailwind CSS Project!</h2>
+    </main>
   </div>
 )
 
