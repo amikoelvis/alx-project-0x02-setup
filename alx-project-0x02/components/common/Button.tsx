fd6746dac19987ff199f7a004846b11483676d81
Amiko Elvis <amikoelvis@gmail.com>
2025-07-24 21:29:49 +0300

    Update Button.tsx to include { type ButtonProps }

--- alx-project-0x02/components/common/Button.tsx
+++ alx-project-0x02/components/common/Button.tsx
@@ -1,4 +1,4 @@
-import { ButtonProps } from "@/interfaces";
+import { type ButtonProps } from "@/interfaces";
 
 const Button: React.FC<ButtonProps> = ({ size, shape, label, onClick }) => {
     // Tailwind class names based on the size and shape props
