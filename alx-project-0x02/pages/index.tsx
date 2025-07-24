57b24d9f4118a5ffd877a7d15a255e7455850020
Amiko Elvis <amikoelvis@gmail.com>
2025-07-24 12:53:11 +0300

    Create the GitHub Repository and Initial Setup and Set Up a Next.js Project

---
+++ alx-project-0x02/pages/index.tsx
@@ -0,0 +1,7 @@
+const Home: React.FC = () => (
+  <div className="p-6">
+      <h1 className="text-xl">Welcome to my Next.js + TypeScript + Tailwind CSS Project!</h1>
+  </div>
+)
+
+export default Home
